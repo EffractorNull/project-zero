a77f4a30504be5fc8a35762dddbb5b59571d5437
root <root@monday.red-soft.biz>
2023-12-22 18:28:28 +0300

    changed test

--- app/test.py
+++ app/test.py
@@ -7,10 +7,10 @@ class TestCase(unittest.TestCase):
         self.app = zero.app.test_client()
     
     def test_get_mainpage(self):
-        page = self.app.post("/", data=dict(name="Joe Biden"))
+        page = self.app.post("/", data=dict(name="Алиса Селезнёва"))
         assert page.status_code == 200
         assert 'Hello' in str(page.data)
-        assert 'Joe Biden' in str(page.data)
+        assert 'Алиса Селезнёва' in str(page.data)
 
     def test_html_esacaping(self):
         page = self.app.post("/", data=dict(name='"><b>TEST</b><!--'))
